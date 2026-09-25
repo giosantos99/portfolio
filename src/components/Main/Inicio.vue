7e29f93199f9877ee3cfa40794f414bd4e621f0b
giosantos99 <giovanna.santos.souza99@gmail.com>
2026-09-25 13:32:50 -0300

    fix: Adiciona extensão do arquivo PDF do currículo

--- src/components/Main/Inicio.vue
+++ src/components/Main/Inicio.vue
@@ -124,7 +124,7 @@ const goContato = () => navigateToSection('contato')
 
 const baixarCurriculo = async () => {
   try {
-    const response = await fetch('pdf/Giovanna Santos de Souza - Desenvolvedora Front-end')
+    const response = await fetch('pdf/Giovanna Santos de Souza - Desenvolvedora Front-end.pdf')
 
     if (!response.ok) throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`)
 
